2f617ace2ea4733d848e9d70bd446d4bd9c9edcf
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-03-19 18:27:43 -0500

    [Fix] bootstrap errors

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,33 @@
 "use strict"
 
+let websites = [
+  "https://www.udemy.com/*",
+  "https://platzi.com/*",
+  "https://keep.google.com/*",
+  "https://stackoverflow.com/*",
+  "https://www.geeksforgeeks.org/*",
+  "https://github.com/*",
+  "https://www.coursera.org/*",
+  "https://www.deepl.com/*",
+  "https://intranet.hbtn.io/",
+  "https://www.digitalocean.com/",
+  "https://www.wikipedia.org/",
+  "https://translate.google.com/",
+];
+
+
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
   var url = tabs[0].url;
-});
+  console.log(url)
+  var n = 0;
+  var l = document.getElementById("timer");
 
-var n = 0;
-var l = document.getElementById("timer");
-window.setInterval(function(){
-  l.innerHTML = n;
-  n++;
-}, 1000);
+  for (let urls in websites) {
+    if (url == websites[urls]) {
+      window.setInterval(function(){
+        l.innerHTML = n;
+        n++;
+      }, 1000);
+    }
+  }
+});
