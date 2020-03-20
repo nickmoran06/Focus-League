b7229fa41dc7dba32bf6a748e56525ea2d213c1c
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-03-20 18:14:07 -0500

    [Feature] Background procedures

--- js/cronometer.js
+++ js/cronometer.js
@@ -1,11 +1,38 @@
 window.onload = init;
+
+console.log('cronometrar');
+
 function init(){
     document.querySelector(".start").addEventListener("click",cronometrar);
     document.querySelector(".stop").addEventListener("click",parar);
     document.querySelector(".reiniciar").addEventListener("click",reiniciar);
+    var browser = browser || chrome;
     h = 0;
     m = 0;
     s = 0;
+
+    browser.runtime.sendMessage({
+        type: "gethAux"
+    }, obj => {
+        h = obj.result;
+
+        console.log(hAux);
+    })
+
+    browser.runtime.sendMessage({
+        type: "getmAux"
+    }, obj => {
+        m = obj.result;
+        console.log(mAux);
+    })
+
+    browser.runtime.sendMessage({
+        type: "getsAux"
+    }, obj => {
+        s = obj.result;
+        console.log(sAux);
+    })
+
     document.getElementById("hms").innerHTML="00:00:00";
 }         
 function cronometrar(){
@@ -14,7 +41,9 @@ function cronometrar(){
     document.querySelector(".start").removeEventListener("click",cronometrar);
 }
 function escribir(){
+    var browser = browser || chrome;
     var hAux, mAux, sAux;
+
     s++;
     if (s>59){m++;s=0;}
     if (m>59){h++;m=0;}
@@ -24,7 +53,22 @@ function escribir(){
     if (m<10){mAux="0"+m;}else{mAux=m;}
     if (h<10){hAux="0"+h;}else{hAux=h;}
 
-    document.getElementById("hms").innerHTML = hAux + ":" + mAux + ":" + sAux; 
+    document.getElementById("hms").innerHTML = hAux + ":" + mAux + ":" + sAux;
+
+    browser.runtime.sendMessage({
+        type: "sethAux",
+        value: h
+    });
+
+    browser.runtime.sendMessage({
+        type: "setmAux",
+        value: m
+    });
+
+    browser.runtime.sendMessage({
+        type: "setsAux",
+        value: s
+    });
 }
 function parar(){
     clearInterval(id);
